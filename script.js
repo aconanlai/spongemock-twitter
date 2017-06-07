422c6fdf99e9b16670e8bf720a742c7feb25bcd3
conan <aconanlai@gmail.com>
2017-06-07 18:15:52 -0400

    get actual tweet content and display image

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-// http://pixel.nymag.com/imgs/daily/vulture/2017/05/16/16-spongebob-explainer.w710.h473.2x.jpg
-
 function addButtons() {
   var actionLists = document.querySelectorAll('.ProfileTweet-actionList');
   actionLists.forEach(function (node) {
     var spongemock = node.querySelector('.spongemock');
     if (!spongemock) {
       var div = document.createElement('div');
-    div.className += 'ProfileTweet-action spongemock';
+      div.className += 'ProfileTweet-action spongemock';
 
-    var button = document.createElement('button');
-    // button.className += 'ProfileTweet-actionButton u-textUserColorHover js-actionButton js-actionShareViaDM';
-    button.className += 'ProfileTweet-actionButton u-textUserColorHover';
-    div.appendChild(button);
+      var button = document.createElement('button');
+      // button.className += 'ProfileTweet-actionButton u-textUserColorHover js-actionButton js-actionShareViaDM';
+      button.className += 'ProfileTweet-actionButton u-textUserColorHover js-actionButton js-actionReply';
+      div.appendChild(button);
 
-    var innerDiv = document.createElement('div');
-    innerDiv.className += 'IconContainer js-tooltip';
-    button.appendChild(innerDiv);
+      var innerDiv = document.createElement('div');
+      innerDiv.className += 'IconContainer js-tooltip';
+      button.appendChild(innerDiv);
 
-    var iconSpan = document.createElement('span');
-    iconSpan.className += 'Icon Icon--retweet';
-    innerDiv.appendChild(iconSpan);
+      var iconSpan = document.createElement('span');
+      iconSpan.className += 'Icon Icon--retweet spongemockButton';
+      innerDiv.appendChild(iconSpan);
 
-    var tooltipSpan = document.createElement('span');
-    tooltipSpan.className += 'u-hiddenVisually';
-    innerDiv.appendChild(tooltipSpan);
+      var tooltipSpan = document.createElement('span');
+      tooltipSpan.className += 'u-hiddenVisually';
+      innerDiv.appendChild(tooltipSpan);
 
-    node.appendChild(div);
+      node.appendChild(div);
     }
   });
 }
@@ -48,3 +46,15 @@ var observerConfig = {
 
 var targetNode = document.querySelector('.js-navigable-stream');
 observer.observe(targetNode, observerConfig);
+
+document.addEventListener('click', function (e) {
+  if (e.target && e.target.className.includes('spongemockButton')) {
+    var textbox = document.querySelector('#tweet-box-global').firstChild;
+    var tweet = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('.js-tweet-text-container').textContent;
+    console.log(tweet);
+    textbox.innerText = 'https://aconanlai.github.io/spongemock/';
+  }
+});
+
+
+// js-tweet-text-container.firstChild.innerText
